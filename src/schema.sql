e39554bb94a6b2d751a01381818dcfb06d573958
victoriajquinto <victoriajquinto@gmail.com>
2023-07-12 12:26:17 -0700

    set up typescript, express server, install dependencies

---
+++ src/schema.sql
@@ -0,0 +1,38 @@
+
+-- CREATE TABLE product (
+--   id PRIMARY KEY,
+--   campus VARCHAR(255),
+--   "name" VARCHAR(255),
+--   slogan VARCHAR(255),
+--   description VARCHAR(255),
+--   category VARCHAR(255),
+--   default_price VARCHAR(255),
+--   created_at VARCHAR(255),
+--   updated_at VARCHAR(255),
+--   features ARRAY,
+-- );
+
+-- CREATE TABLE style (
+--   style_id INT,
+--   name VARCHAR(255),
+--   original_price VARCHAR(255),
+--   sale_price VARCHAR(255),
+--   default TINYINT
+-- );
+
+-- CREATE TABLE ratings (
+--   one INT,
+--   two INT,
+--   three INT,
+--   four INT,
+--   five INT,
+-- );
+
+-- CREATE TABLE (
+--   true INT
+--   false INT
+-- );
+
+DROP DATABASE sdc;
+CREATE DATABASE sdc;
+
