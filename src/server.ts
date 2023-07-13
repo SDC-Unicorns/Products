40b9928fd7d2c4a30573e478bf942f926cb5b2b8
victoriajquinto <victoriajquinto@gmail.com>
2023-07-13 15:33:08 -0700

    Add pg dependency, set up db connection with pg

--- src/server.ts
+++ src/server.ts
@@ -5,13 +5,17 @@ const dotenv = require('dotenv');
 const path = require('path');
 const morgan = require('morgan');
 const cors = require('cors');
+const pool = require('./db');
 
 const app = express();
 app.use(express.json());
-app.use(morgan('dev'));
 app.use(cors());
+app.use(morgan('dev'));
 
 const PORT = process.env.PORT || 3000;
+
+//ROUTES//
+
 
 app.listen(PORT, () => {
   console.log(`server has started on port ${PORT}`)
