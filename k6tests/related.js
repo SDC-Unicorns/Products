970f21e66110aa6f4ffcf0677279f183a11b7244
victoriajquinto <victoriajquinto@gmail.com>
2023-07-17 22:01:06 -0700

    create k6 test scripts

---
+++ k6tests/related.js
@@ -0,0 +1,17 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export let options = {
+  stages: [
+    { duration: '2m', target: 100 } // Ramp up to 100 requests per second over 1 minute
+  ],
+};
+
+export default function () {
+  let min = 900000;
+  let max = 1000011;
+  let product_id = Math.floor(Math.random() * (max - min)) + min;
+  let res = http.get(`http://localhost:9000/products/${product_id}/related`);
+
+  sleep(1); // Sleep for 1 second to maintain the desired throughput
+}
