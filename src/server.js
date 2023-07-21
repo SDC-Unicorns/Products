0b2f6aa5d10ef23f9a58d489884c3b69a62c8a33
victoriajquinto <victoriajquinto@gmail.com>
2023-07-20 20:37:20 -0700

    remove loader.io verification path to prep for nginx config

--- src/server.js
+++ src/server.js
@@ -119,21 +119,14 @@ app.get('/products/:product_id/related', async(req, res) => {
   }
 });
 
-// app.get('/loaderio-151677548dc745b7ce23f60ea89d7358/', async (req, res) => {
+
+// app.get(`${process.env.LOADER_IO_ENDPOINT}`, async (req, res) => {
 //   try{
 //     res.status(200).send(process.env.LOADER_IO_TOKEN);
 //   } catch (error) {
 //     console.log('error in loaderio verification', error);
 //   };
 // });
-
-app.get(`${process.env.LOADER_IO_ENDPOINT}`, async (req, res) => {
-  try{
-    res.status(200).send(process.env.LOADER_IO_TOKEN);
-  } catch (error) {
-    console.log('error in loaderio verification', error);
-  };
-});
 
 app.listen(PORT, () => {
   console.log(`server has started on port ${PORT}`)
